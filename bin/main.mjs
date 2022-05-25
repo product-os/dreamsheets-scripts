a4edda935fc2293bff725c29822b2f30df4f34e9
Somo S <somo@mombo.solutions>
2022-05-25 13:16:39 +0000

    support setting push scriptId via ENV_VAR
    Change-type: patch
    Signed-off-by: Somo S <somo@mombo.solutions>

--- bin/main.mjs
+++ bin/main.mjs
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
-
-// require('../dist/index.js')
-// await import('../dist/index.mjs')
-// import '../dist/index.mjs';
-
 process.on('unhandledRejection', err => { throw err })
-
+import 'dotenv/config'
 import { push, build, test } from '../dist/index.mjs'
 import webpackConfig from '../config/webpack.config.mjs'
 import jestConfig from '../config/jest.config.cjs'
@@ -15,7 +10,7 @@ import {hideBin} from 'yargs/helpers';
 
 const NOP = () => {}
 await yargs(hideBin(process.argv))
-	.command('push <scriptId>', 'push the bundle', NOP, async ({ scriptId }) => await push(scriptId))
+	.command('push [scriptId]', 'push the bundle', NOP, async ({ scriptId }) => push(scriptId ?? process?.env.DSX_SCRIPT_ID))
 	.command('build', 'build the source', NOP, async () => build(webpackConfig))
 	.command('test', 'test the source', NOP, async () => test(jestConfig))
 	.help('h')
